1eb1998d19b2b514753a109a3a94d292d1fc60db
6qingqing <1643556831@qq.com>
2017-04-23 16:13:11 +0800

    1

---
+++ triangle/triangle.js
@@ -0,0 +1,22 @@
+var triangle = {
+    isEquilateral : function(a,b,c) {
+    	    if(a!=b) return false ; 
+    	    if(b!=c) return false ;
+    	    if(a<=0) return false ;
+    		return true ;
+    },
+    isIsosceles: function (a, b, c) {
+        //if (a + b < c && a + c < b && b + c < a && Math.abs(a - b) < c && Math.abs(a - c) < b && Math.abs(b - c) < a)
+            if (a + b < c) return false;
+            if (a != b && a != c && b != c) return false;
+            if (a <= 0) return false;
+            return true;
+        
+    },
+    isScalene:function(a,b,c){
+        //if (a + b < c && a + c < b && b + c < a && Math.abs(a - b) < c && Math.abs(a - c) < b && Math.abs(b - c) < a)
+            if (a != b || a != c || b != c) return true;
+            return false;
+    }
+};
+
